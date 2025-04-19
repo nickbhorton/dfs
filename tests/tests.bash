21abc80c1780dcda15872caffb45d51d16b9de67
Nick Horton <nickkbhorton@gmail.com>
2025-04-19 15:41:19 +0000

    adding a real bash test

--- tests/tests.bash
+++ tests/tests.bash
@@ -1,7 +1,49 @@
 #!/bin/bash
-cat tests/put.dat | nc 127.0.0.1 10001
-# -q 0 closes socket after done sending
-cat tests/put_incomplete.dat | nc -q 0 127.0.0.1 10001 # should fail
-cat tests/put.dat | nc 127.0.0.1 10001
-cat tests/test.dat | nc 127.0.0.1 10001 | hexdump -X
-cat tests/test_exists.dat | nc 127.0.0.1 10001 | hexdump -X
+
+# setup
+./dfs ./.test_dfs1/ 5000 1> .dfs_log.txt &
+dfs_pid=$!
+
+# tests
+ 
+rv=$(./testcli 5000 tests/test_file1.txt TEST)
+if [ "$rv" != "n" ]; then
+    echo "test_file1.txt TEST before put failed"
+fi
+
+rv=$(./testcli 5000 tests/test_file2.jpg TEST)
+if [ "$rv" != "n" ]; then
+    echo "tests/test_file2.jpg TEST before put failed"
+fi
+
+./testcli 5000 tests/test_file1.txt PUT
+rv=$(./testcli 5000 tests/test_file1.txt TEST)
+if [ "$rv" != "y" ]; then
+    echo "test_file1.txt TEST after put failed"
+fi
+
+./testcli 5000 tests/test_file2.jpg PUT
+rv=$(./testcli 5000 tests/test_file2.jpg TEST)
+if [ "$rv" != "y" ]; then
+    echo "test_file2.jpg TEST after put failed"
+fi
+
+./testcli 5000 tests/test_file1.txt GET > .temp
+if ! cmp -s .temp tests/test_file1.txt; then
+    echo "test_file1.txt GET failed"
+fi
+rm .temp
+
+./testcli 5000 tests/test_file2.jpg GET > .temp
+if ! cmp -s .temp tests/test_file2.jpg; then
+    echo "test_file2.jpg GET failed"
+fi
+rm .temp
+
+# kill the server
+kill $dfs_pid
+# print the log
+echo "dfs log:"
+cat .dfs_log.txt
+rm .dfs_log.txt
+rm -rf ./.test_dfs1/
